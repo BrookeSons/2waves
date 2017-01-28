a92f4129625ab05cfcaceb8fb2e88bf3a6be4c03
tbrooke <tom.brooke@gmail.com>
2017-01-27 23:56:40 -0500

    landing page styling

--- src/waves/site.clj
+++ src/waves/site.clj
@@ -8,17 +8,22 @@
       [:meta {:charset "utf-8"}]
       [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
       [:title "2Waves Media"]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
+      [:meta {:name "viewport"   :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
       [:meta {:itemprop "author" :name "author" :content "2Waves (Rchard.Brooke@2wavesmeda.com)"}]
-      [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/icon?family=Material+Icons"}]
+      [:link {:rel "stylesheet"  :href "https://fonts.googleapis.com/icon?family=Material+Icons"}]
       [:link {:rel "stylesheet"  :href "https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"}]
-      (include-js   "https://code.getmdl.io/1.3.0/material.min.js")
+      [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Baumans" }]
+       (include-js   "https://code.getmdl.io/1.3.0/material.min.js")
       (include-css "/index.css")
       [:link {:href "https://fonts.googleapis.com/css?family=Oswald" :rel "stylesheet" :type "text/css"}]]
     [:body
+     [:div.top
+      [:ul
+       [:li "Contact Us"]
+       [:li "Our Work"]]]
       [:div.hero
-        ; [:h1 "2Waves"]
        [:img {:src "/images/waves.png" :height "255px"}]
+        ; [:h1 "2Waves"]
        ]
       [:section.docs-section
         [:h2.waves "2Waves Media"]
